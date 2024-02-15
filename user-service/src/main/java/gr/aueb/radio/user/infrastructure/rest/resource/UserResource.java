b2343b16434d376e62b26363c538ff354f86231d
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-16 01:37:12 +0200

    Add UserResource

---
+++ user-service/src/main/java/gr/aueb/radio/user/infrastructure/rest/resource/UserResource.java
@@ -0,0 +1,52 @@
+package gr.aueb.radio.user.infrastructure.rest.resource;
+
+import gr.aueb.radio.user.application.UserService;
+import gr.aueb.radio.user.common.RadioException;
+import gr.aueb.radio.user.domain.user.User;
+import gr.aueb.radio.user.infrastructure.rest.ApiPath.Root;
+import gr.aueb.radio.user.infrastructure.rest.representation.UserInputDTO;
+import gr.aueb.radio.user.infrastructure.rest.representation.UserMapper;
+import gr.aueb.radio.user.infrastructure.rest.representation.UserRepresentation;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+
+import java.net.URI;
+
+@RequestScoped
+@Path(Root.USERS)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @Inject
+    UserService userService;
+
+    @Inject
+    UserMapper userMapper;
+
+    @GET
+    @Path("/{id}")
+    public Response getUser(@PathParam("id") Integer id){
+        try {
+            UserRepresentation userRepresentation = userService.findUser(id);
+            return Response.ok().entity(userRepresentation).build();
+        }catch (NotFoundException nfe){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    @POST
+    public Response register(@Valid UserInputDTO userRepresentation){
+        try {
+            User user = userService.create(userRepresentation.toRepresentation());
+            URI uri = UriBuilder.fromResource(UserResource.class).path(String.valueOf(user.getId())).build();
+            return Response.created(uri).entity(userMapper.toRepresentation(user)).build();
+        }catch (RadioException re){
+            return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(re.getMessage()).build();
+        }
+    }
+}
