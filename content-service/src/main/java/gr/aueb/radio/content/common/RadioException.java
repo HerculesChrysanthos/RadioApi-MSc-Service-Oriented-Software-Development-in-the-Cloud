2c817a7984adc624325eb7f0611263ae8f24e941
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-17 14:41:38 +0200

    Add common exceptions

---
+++ content-service/src/main/java/gr/aueb/radio/content/common/RadioException.java
@@ -0,0 +1,8 @@
+package gr.aueb.radio.content.common;
+
+@SuppressWarnings("serial")
+public class RadioException extends RuntimeException {
+    public RadioException(String message) {
+        super(message);
+    }
+}
