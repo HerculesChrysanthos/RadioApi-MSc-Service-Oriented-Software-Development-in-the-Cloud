153d884684304cfbca8b76b515d548b587352ed5
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-17 14:40:36 +0200

    Add ApiPath for content

---
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/rest/ApiPath.java
@@ -0,0 +1,13 @@
+package gr.aueb.radio.content.infrastructure.rest;
+
+public class ApiPath {
+
+    public static final String ROOT = "/api";
+
+    public static class Root {
+        public static final String SONGS = ROOT + "/songs";
+        public static final String ADS = ROOT + "/ads";
+        public static final String GENRES = ROOT + "/genres";
+    }
+
+}
