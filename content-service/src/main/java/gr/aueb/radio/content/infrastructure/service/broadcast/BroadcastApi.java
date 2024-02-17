be793a6059e737ccb05907425c2905ce7346b201
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-17 14:42:16 +0200

    Init content-service interface clients

---
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/service/broadcast/BroadcastApi.java
@@ -0,0 +1,4 @@
+package gr.aueb.radio.content.infrastructure.service.broadcast;
+
+public interface BroadcastApi {
+}
---
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/service/user/UserApi.java
@@ -0,0 +1,4 @@
+package gr.aueb.radio.content.infrastructure.service.user;
+
+public interface UserApi {
+}
