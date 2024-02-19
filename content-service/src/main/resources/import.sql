680e2da8a2afa6115cf5ec477717fdaa6c53457f
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-19 20:40:21 +0200

    Add ads

--- content-service/src/main/resources/import.sql
+++ content-service/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 delete from ads;
 delete from songs;
+
+insert into ads (id, duration, rep_per_zone, starting_date, ending_date, timezone) values (1001, 15, 0, PARSEDATETIME('01-01-2022','dd-MM-yyyy'), PARSEDATETIME('01-03-2022','dd-MM-yyyy'), 'LateNight');
+insert into ads (id, duration, rep_per_zone, starting_date, ending_date, timezone) values (1002, 30, 0, PARSEDATETIME('01-01-2022','dd-MM-yyyy'), PARSEDATETIME('01-04-2022','dd-MM-yyyy'), 'LateNight');
