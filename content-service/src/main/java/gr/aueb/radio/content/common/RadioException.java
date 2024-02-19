33ae2390d7ff5a6fc543a31356e1ef4bbd5e76cb
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-19 19:06:54 +0200

    Add constructor

--- content-service/src/main/java/gr/aueb/radio/content/common/RadioException.java
+++ content-service/src/main/java/gr/aueb/radio/content/common/RadioException.java
@@ -3,7 +3,13 @@ package gr.aueb.radio.content.common;
 @SuppressWarnings("serial")
 public class RadioException extends RuntimeException {
 
-    private final int statusCode;
+    private int statusCode;
+
+    public RadioException(){}
+
+    public RadioException(String message) {
+        super(message);
+    }
     public RadioException(String message, int statusCode) {
         super(message);
         this.statusCode = statusCode;
