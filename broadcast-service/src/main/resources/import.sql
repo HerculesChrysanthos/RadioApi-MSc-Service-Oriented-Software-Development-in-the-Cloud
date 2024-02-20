d44cf71ee3e4a210722ed1f1f319193e76a2c375
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-20 21:02:12 +0200

    Add data

--- broadcast-service/src/main/resources/import.sql
+++ broadcast-service/src/main/resources/import.sql
@@ -1,3 +1,11 @@
 delete from ad_broadcasts;
 delete from song_broadcasts;
 delete from broadcasts;
+
+insert into broadcasts (id, duration, starting_date, starting_time, type, user_id, genre_id) values(3001, 200, PARSEDATETIME('01-02-2022','dd-MM-yyyy'), '00:00', 'PLAYLIST', 6001, 1);
+
+insert into ad_broadcasts (id, broadcast_date, broadcast_time, ad_id, broadcast_id) values (4001, PARSEDATETIME('01-02-2022','dd-MM-yyyy'), '00:40', 1001, 3001);
+insert into ad_broadcasts (id, broadcast_date, broadcast_time, ad_id, broadcast_id) values (4002, PARSEDATETIME('01-02-2022','dd-MM-yyyy'), '01:50', 1002, 3001);
+
+insert into song_broadcasts (id, broadcast_date, broadcast_time, song_id, broadcast_id) values (5001, PARSEDATETIME('01-02-2022','dd-MM-yyyy'), '00:00', 2001, 3001);
+insert into song_broadcasts (id, broadcast_date, broadcast_time, song_id, broadcast_id) values (5002, PARSEDATETIME('01-02-2022','dd-MM-yyyy'), '01:10', 2002, 3001);
