bed3dd3c49508ce608d1dc6beff4cdd53dde61b9
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-20 02:52:34 +0200

    Update RadioExceptions

--- user-service/src/main/java/gr/aueb/radio/user/common/RadioException.java
+++ user-service/src/main/java/gr/aueb/radio/user/common/RadioException.java
@@ -2,7 +2,20 @@ package gr.aueb.radio.user.common;
 
 @SuppressWarnings("serial")
 public class RadioException extends RuntimeException {
+
+    private int statusCode;
+
+    public RadioException(){}
+
     public RadioException(String message) {
         super(message);
+    }
+    public RadioException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
