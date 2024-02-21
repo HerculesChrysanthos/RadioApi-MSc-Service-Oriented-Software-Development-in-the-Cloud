df4835b48cf84e02f51e8fe9ca7aea5954fd6249
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-21 03:49:55 +0200

    Add repository queries

--- broadcast-service/src/main/java/gr/aueb/radio/broadcast/infrastructure/persistence/SongBroadcastRepository.java
+++ broadcast-service/src/main/java/gr/aueb/radio/broadcast/infrastructure/persistence/SongBroadcastRepository.java
@@ -16,18 +16,23 @@ public class SongBroadcastRepository implements PanacheRepositoryBase<SongBroadc
     public List<SongBroadcast> findByDateDetails(LocalDate date){
         Map<String, Object> params = new HashMap<>();
         params.put("date", date);
-        List<SongBroadcast> broadcasts = find("select sb from SongBroadcast sb left join fetch sb.song where sb.broadcastDate=:date", params).list();
-        return find("select sb from SongBroadcast sb left join fetch sb.broadcast where sb in :song_broadcasts", Parameters.with("song_broadcasts", broadcasts).map()).list();
+//        List<SongBroadcast> broadcasts = find("select sb from SongBroadcast sb left join fetch sb.song where sb.broadcastDate=:date", params).list();
+//        return find("select sb from SongBroadcast sb left join fetch sb.broadcast where sb in :song_broadcasts", Parameters.with("song_broadcasts", broadcasts).map()).list();
+
+        return find("select sb from SongBroadcast sb left join fetch sb.broadcast where sb.broadcastDate=:date", params).list();
     }
 
     public SongBroadcast findByIdDetails(Integer id){
         Map<String, Object> params = new HashMap<>();
         params.put("id", id);
-        List<SongBroadcast> broadcasts = find("select sb from SongBroadcast sb left join fetch sb.song where sb.id=:id", params).list();
-        try {
-            return find("select sb from SongBroadcast sb left join fetch sb.broadcast where sb in :song_broadcasts", Parameters.with("song_broadcasts", broadcasts).map()).singleResult();
-        }catch (NoResultException e){
-            return null;
-        }
+        //return find("select sb from SongBroadcast sb where sb.id=:id", params).singleResult();
+
+        return find("select sb from SongBroadcast sb left join fetch sb.broadcast where sb.id=:id",params).singleResult();
+//        List<SongBroadcast> broadcasts = find("select sb from SongBroadcast sb left join fetch sb.song where sb.id=:id", params).list();
+//        try {
+//            return find("select sb from SongBroadcast sb left join fetch sb.broadcast where sb in :song_broadcasts", Parameters.with("song_broadcasts", broadcasts).map()).singleResult();
+//        }catch (NoResultException e){
+//            return null;
+//        }
     }
 }
