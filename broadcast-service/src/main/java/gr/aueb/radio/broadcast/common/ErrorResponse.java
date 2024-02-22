dec91250e72a3525408672f321c0ba2d101698d7
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-22 03:01:52 +0200

    Add error response entity in case we want to return obj on returned body error

---
+++ broadcast-service/src/main/java/gr/aueb/radio/broadcast/common/ErrorResponse.java
@@ -0,0 +1,22 @@
+package gr.aueb.radio.broadcast.common;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse() {
+
+    }
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
