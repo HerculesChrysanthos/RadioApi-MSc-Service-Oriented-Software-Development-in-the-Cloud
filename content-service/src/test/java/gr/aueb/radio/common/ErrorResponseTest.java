7e10ee068d8843c5e9f7d8c6a7639e963313757f
kostassifa <sifavs@yahoo.com>
2024-02-22 23:29:18 +0200

    junit content test

---
+++ content-service/src/test/java/gr/aueb/radio/common/ErrorResponseTest.java
@@ -0,0 +1,26 @@
+package gr.aueb.radio.common;
+
+import gr.aueb.radio.content.common.ErrorResponse;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ErrorResponseTest {
+
+
+    @Test
+    public void testConstructor() {
+        String errorMessage = "Test error message";
+        ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+        assertEquals(errorMessage, errorResponse.getMessage());
+    }
+
+    @Test
+    public void testSetMessage() {
+        ErrorResponse errorResponse = new ErrorResponse();
+        String errorMessage = "Test error message";
+        errorResponse.setMessage(errorMessage);
+        assertEquals(errorMessage, errorResponse.getMessage());
+    }
+
+}
--- content-service/src/test/java/gr/aueb/radio/domain/song/SongTest.java
+++ content-service/src/test/java/gr/aueb/radio/domain/song/SongTest.java
@@ -1,9 +1,18 @@
 package gr.aueb.radio.domain.song;
 
+import gr.aueb.radio.content.common.DateUtil;
 import gr.aueb.radio.content.domain.genre.Genre;
 import gr.aueb.radio.content.domain.song.Song;
+import gr.aueb.radio.content.infrastructure.persistence.SongRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import jakarta.inject.Inject;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SongTest {
 
@@ -20,12 +29,24 @@ public class SongTest {
         Song song = new Song(title, genre, duration, artist, year);
 
         // Assert
-        Assertions.assertNotNull(song);
-        Assertions.assertEquals(title, song.getTitle());
-        Assertions.assertEquals(genre, song.getGenre());
-        Assertions.assertEquals(duration, song.getDuration());
-        Assertions.assertEquals(artist, song.getArtist());
-        Assertions.assertEquals(year, song.getYear());
+        assertNotNull(song);
+        assertEquals(title, song.getTitle());
+        assertEquals(genre, song.getGenre());
+        assertEquals(duration, song.getDuration());
+        assertEquals(artist, song.getArtist());
+        assertEquals(year, song.getYear());
+    }
+    @Inject
+    SongRepository songRepository;
+
+    @Test
+    public void testGetSongById() {
+
+        Song song = new Song("Title", null, 300, "Artist", 2022);
+
+
+        assertEquals(song.getId(), song.getId());
+
     }
 
     @Test
@@ -38,7 +59,7 @@ public class SongTest {
         song.addSongBroadcast(broadcastId);
 
         // Assert
-        Assertions.assertEquals(1, song.getSongBroadcasts().size());
+        assertEquals(1, song.getSongBroadcasts().size());
         Assertions.assertTrue(song.getSongBroadcasts().contains(broadcastId));
     }
 
@@ -47,6 +68,77 @@ public class SongTest {
         Song song = new Song();
         String artist = "New Artist";
         song.setArtist(artist);
-        Assertions.assertEquals(artist, song.getArtist());
+        assertEquals(artist, song.getArtist());
     }
+
+    Song song;
+    private static LocalDate localDate = DateUtil.setDate("01-01-2023");
+    private static LocalTime localTime = DateUtil.setTime("01:01");
+    private static String title = "Title";
+
+    private static int duration = 10;
+    private static String artist = "Artist";
+    private static Integer year = 2021;
+
+    @BeforeEach
+    public void setUp() {
+        song = new Song();
+        song.setTitle(title);
+        song.setDuration(duration);
+        song.setArtist(artist);
+        song.setYear(year);
+    }
+
+    @Test
+    public void ValidSong() throws Exception {
+        Genre genre = new Genre();
+        Song song = new Song("Title",genre,180, "Artist",2021);
+        assertEquals(song.getArtist(),("Artist"));
+    }
+
+    @Test
+    public void successfulSetUp() {
+        Genre genre = new Genre();
+        assertEquals(song.getTitle(), title);
+        assertNull(song.getGenre());
+        assertEquals(song.getArtist(), artist);
+        assertEquals(song.getYear(), year);
+        assertEquals(song.getDuration(), duration);
+    }
+
+    @Test
+    public void setTitleTest() {
+        String newTitle = "New Title";
+        song.setTitle(newTitle);
+        assertEquals(newTitle, song.getTitle());
+    }
+
+    @Test
+    public void setGenreTest() {
+        Genre genre = new Genre();
+        song.setGenre(genre);
+        assertEquals(genre, song.getGenre());
+    }
+
+    public void setArtistTest() {
+        String newArtist = "New Artist";
+        song.setArtist(newArtist);
+        assertEquals(newArtist, song.getArtist());
+    }
+
+    @Test
+    public void setYearTest() {
+        Integer newYear = 2022;
+        song.setYear(newYear);
+        assertEquals(newYear, song.getYear());
+    }
+
+    @Test
+    public void setDurationTest() {
+        int newDuration = 240;
+        song.setDuration(newDuration);
+        assertEquals(newDuration, song.getDuration());
+    }
+
+
 }
