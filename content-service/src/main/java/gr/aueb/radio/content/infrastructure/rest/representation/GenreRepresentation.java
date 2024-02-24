c100b30ce868ff6902e59c054b5b7d759130a7ee
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-24 16:10:13 +0200

    Add annotation

--- content-service/src/main/java/gr/aueb/radio/content/infrastructure/rest/representation/GenreRepresentation.java
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/rest/representation/GenreRepresentation.java
@@ -1,5 +1,8 @@
 package gr.aueb.radio.content.infrastructure.rest.representation;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public class GenreRepresentation {
 
     public Integer Id;
