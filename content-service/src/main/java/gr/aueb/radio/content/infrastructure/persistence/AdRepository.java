43c82f5989947f19dfa15732f53fcfbf4abdebda
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-25 17:57:13 +0200

    Add findByFilters query in ad repo

--- content-service/src/main/java/gr/aueb/radio/content/infrastructure/persistence/AdRepository.java
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/persistence/AdRepository.java
@@ -4,11 +4,16 @@ import gr.aueb.radio.content.domain.ad.Ad;
 import gr.aueb.radio.content.domain.ad.Zone;
 import gr.aueb.radio.content.domain.song.Song;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.RequestScoped;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @RequestScoped
 public class AdRepository implements PanacheRepositoryBase <Ad, Integer> {
 
@@ -19,6 +24,24 @@ public class AdRepository implements PanacheRepositoryBase <Ad, Integer> {
     public List<Ad> findAdsByIds(List<Integer> ids) {
         PanacheQuery<Ad> query = find("select a from Ad a where a.id IN :ids ", Parameters.with("ids", ids).map());
 
+        return query.list();
+    }
+
+    public List<Ad> findByFilters(LocalDate date, String timezone) {
+        Map<String, Object> params = new HashMap<>();
+        StringBuilder queryBuilder = new StringBuilder("select ad from Ad ad left join fetch ab.broadcast where");
+
+        if (date != null) {
+            queryBuilder.append(" and ad.broadcastDate = :date");
+            params.put("date", date);
+        }
+
+        if (timezone != null && !timezone.isEmpty()) {
+            queryBuilder.append(" and ad.timezone = :timezone");
+            params.put("timezone", timezone);
+        }
+
+        PanacheQuery<Ad> query = find(queryBuilder.toString(), params);
         return query.list();
     }
 
