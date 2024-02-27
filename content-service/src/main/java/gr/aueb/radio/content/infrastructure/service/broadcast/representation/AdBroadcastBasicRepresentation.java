ab038ff55a9b8d172e3282f833c85ed00a52f0a9
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-27 03:05:52 +0200

    Add broadcasts representations into content service

---
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/service/broadcast/representation/AdBroadcastBasicRepresentation.java
@@ -0,0 +1,9 @@
+package gr.aueb.radio.content.infrastructure.service.broadcast.representation;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class AdBroadcastBasicRepresentation {
+
+    public Integer id;
+}
