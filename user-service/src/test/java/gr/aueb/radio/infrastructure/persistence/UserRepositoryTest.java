253d7dc73cf45bca4c010f76bf54ad9ebfb405e9
HerculesChrysanthos <hercsma@hotmail.com>
2024-02-28 02:42:21 +0200

    Add UserRepositoryTest

---
+++ user-service/src/test/java/gr/aueb/radio/infrastructure/persistence/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package gr.aueb.radio.infrastructure.persistence;
+
+import gr.aueb.radio.common.JPATest;
+import gr.aueb.radio.user.domain.user.User;
+import gr.aueb.radio.user.infrastructure.persistence.UserRepository;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+public class UserRepositoryTest extends JPATest {
+
+    @Inject
+    UserRepository userRepository;
+
+    @Test
+    public void testFindByUsername() {
+        User user = userRepository.findByUsername("producer");
+        assertEquals(6001, user.getId());
+    }
+
+    @Test
+    public void testFindByEmail() {
+        User user = userRepository.findByEmail("producer@email.com");
+        assertEquals(6001, user.getId());
+    }
+}
