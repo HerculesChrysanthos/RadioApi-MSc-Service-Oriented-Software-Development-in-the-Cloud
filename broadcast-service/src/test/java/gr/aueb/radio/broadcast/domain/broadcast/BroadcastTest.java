ee281ea0d07491071dfb867ad0bd8538dd1e61e7
kostassifa <sifavs@yahoo.com>
2024-03-01 20:52:48 +0200

    broadcast domain

--- broadcast-service/src/test/java/gr/aueb/radio/broadcast/domain/broadcast/BroadcastTest.java
+++ broadcast-service/src/test/java/gr/aueb/radio/broadcast/domain/broadcast/BroadcastTest.java
@@ -1,13 +1,17 @@
 package gr.aueb.radio.broadcast.domain.broadcast;
 
+import gr.aueb.radio.broadcast.application.AdBroadcastService;
 import gr.aueb.radio.broadcast.common.DateUtil;
 import gr.aueb.radio.broadcast.domain.adBroadcast.AdBroadcast;
+import gr.aueb.radio.broadcast.infrastructure.service.content.representation.AdBasicRepresentation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +22,7 @@ public class BroadcastTest {
 
     Broadcast broadcast;
     private static LocalDate date = DateUtil.setDate("01-01-2023");
-    private static LocalTime time = DateUtil.setTime("00:00");
+    private static LocalTime time = DateUtil.setTime("13:14");
     private static Integer duration = 100;
     @BeforeEach
     public void setUp(){
@@ -64,4 +68,90 @@ public class BroadcastTest {
         LocalDateTime endingTime = broadcast.getBroadcastEndingDateTime();
         assertTrue(expectedTime.isEqual(endingTime));
     }
+
+    @Test
+    public void toBeBroadcastedSongTest()
+    {
+
+        Broadcast broadcast=new Broadcast();
+        List<AdBroadcast> broadcasts = broadcast.getAdBroadcasts();
+        System.out.println("Size of broadcasts list: " + broadcasts.size());
+        boolean result=broadcast.toBeBroadcastedSong(date,time,broadcasts);
+        assertTrue(result);
+
+    }
+
+    @Test
+    public  void toBeBroadcastedAdTest()
+    {
+        String dateString = "01-02-2022";
+        String dateString_2 = "02-02-2022";
+
+        // Define the pattern
+        String pattern = "dd-MM-yyyy";
+
+        // Create a DateTimeFormatter with the specified pattern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+
+        // Parse the date string into a LocalDate object
+        LocalDate date_1 = LocalDate.parse(dateString, formatter);
+        LocalDate date_2 = LocalDate.parse(dateString_2, formatter);
+        Broadcast broadcast=new Broadcast();
+        AdBasicRepresentation adBasicRepresentation=new AdBasicRepresentation();
+        adBasicRepresentation.startingDate=date_1;
+        adBasicRepresentation.endingDate=date_2;
+        List<AdBroadcast> broadcasts = broadcast.getAdBroadcasts();
+        boolean result=broadcast.toBeBroadcastedAd(date,adBasicRepresentation,broadcasts);
+        assertFalse(result);
+    }
+
+//    @Test
+//    public void createAdBroadcast()
+//    {
+//
+//    }
+
+    @Test
+    public void adCanBeAdded()
+    {
+        String dateString = "01-02-2022";
+        String dateString_2 = "02-02-2022";
+
+        // Define the pattern
+        String pattern = "dd-MM-yyyy";
+
+        // Create a DateTimeFormatter with the specified pattern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+
+        // Parse the date string into a LocalDate object
+        LocalDate date_1 = LocalDate.parse(dateString, formatter);
+        LocalDate date_2 = LocalDate.parse(dateString_2, formatter);
+
+        AdBasicRepresentation adBasicRepresentation=new AdBasicRepresentation();
+        adBasicRepresentation.id=1;
+        adBasicRepresentation.timezone= Zone.EarlyMorning.name();
+        adBasicRepresentation.startingDate=date_1;
+        adBasicRepresentation.endingDate=date_2;
+        adBasicRepresentation.duration=200;
+        List<AdBroadcast> broadcasts = broadcast.getAdBroadcasts();
+        List<AdBasicRepresentation> adBasicRepresentationlist = new ArrayList<>();
+        adBasicRepresentationlist.add(adBasicRepresentation);
+
+        Broadcast broadcast=new Broadcast();
+
+     //   boolean result=broadcast.adCanBeAdded(adBasicRepresentation,time,broadcasts,adBasicRepresentationlist);
+      //  assertFalse(result);
+    }
+
+    @Test
+    public void Broadcast()
+    {
+        broadcast = new Broadcast(duration,date,time,BroadcastType.PLAYLIST);
+        broadcast.setDuration(duration);
+        broadcast.setStartingDate(date);
+        broadcast.setStartingTime(time);
+        broadcast.setType(BroadcastType.PLAYLIST);
+    }
+
+
 }
