dbf977e46b36ba6842ca5ed5adbf6649b8b61abd
despknkd <des.kanakidou@gmail.com>
2024-03-01 15:35:22 +0200

    Ad resource tests addition

--- content-service/src/test/java/gr/aueb/radio/content/infrastructure/rest/resource/AdResourceTest.java
+++ content-service/src/test/java/gr/aueb/radio/content/infrastructure/rest/resource/AdResourceTest.java
@@ -1,124 +1,192 @@
 package gr.aueb.radio.content.infrastructure.rest.resource;
 
+import gr.aueb.radio.content.Fixture;
 import gr.aueb.radio.content.application.AdService;
+import gr.aueb.radio.content.application.UserService;
+import gr.aueb.radio.content.common.ExternalServiceException;
+import gr.aueb.radio.content.common.IntegrationBase;
+import gr.aueb.radio.content.common.RadioException;
 import gr.aueb.radio.content.domain.ad.Ad;
 import gr.aueb.radio.content.domain.ad.Zone;
-import gr.aueb.radio.content.infrastructure.rest.representation.AdInputDTO;
-import gr.aueb.radio.content.infrastructure.rest.representation.AdMapper;
-import gr.aueb.radio.content.infrastructure.rest.representation.AdRepresentation;
+import gr.aueb.radio.content.domain.song.Song;
+import gr.aueb.radio.content.infrastructure.persistence.AdRepository;
+import gr.aueb.radio.content.infrastructure.rest.representation.*;
+import gr.aueb.radio.content.infrastructure.service.user.representation.UserVerifiedRepresentation;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
 
-class AdResourceTest {
+@QuarkusTest
+class AdResourceTest extends IntegrationBase {
+    @Inject
+    AdRepository adRepository;
 
-    @Mock
-    private UriInfo mockUriInfo;
-    @Mock
-    private AdMapper mockAdMapper;
-    @Mock
-    private AdService mockAdService;
-
-    private AdResource adResourceUnderTest;
-
-    private AutoCloseable mockitoCloseable;
+    @InjectMock
+    UserService userService;
 
     @BeforeEach
-    void setUp() {
-        mockitoCloseable = openMocks(this);
-        adResourceUnderTest = new AdResource();
-        adResourceUnderTest.uriInfo = mockUriInfo;
-        adResourceUnderTest.adMapper = mockAdMapper;
-        adResourceUnderTest.adService = mockAdService;
-    }
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        UserVerifiedRepresentation user = new UserVerifiedRepresentation();
+        user.id = 1;
+        user.role = "PRODUCER";
+        Mockito.when(userService.verifyAuth(anyString())).thenReturn(user);
 
-    @AfterEach
-    void tearDown() throws Exception {
-        mockitoCloseable.close();
     }
 
     @Test
-    void testGetAd() {
-        // Setup
-        when(mockAdService.findAd(0, "auth")).thenReturn(new AdRepresentation());
+    public void testFindAd() {
+        // find valid ad in DB to search
+        Ad ad = adRepository.listAll().get(0);
+        // send get request to find the ad
+        String url = Fixture.API_ROOT + Fixture.ADS + "/" + ad.getId();
+        AdRepresentation foundAd = given()
+                .contentType(ContentType.JSON)
+                .when().get(url)
+                .then().statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(AdRepresentation.class);
+        // verify that the found song matches the original song form db
+        assertEquals(foundAd.timezone, ad.getTimezone());
+        assertEquals(foundAd.duration, ad.getDuration());
+        assertEquals(foundAd.repPerZone, ad.getRepPerZone());
 
-        // Run the test
-        final Response result = adResourceUnderTest.getAd(0, "auth");
+        // try to find a song that does not exist
+        url = Fixture.API_ROOT + Fixture.ADS + "/-1";
+        given()
+                .contentType(ContentType.JSON)
+                .when().get(url)
+                .then().statusCode(Response.Status.NOT_FOUND.getStatusCode());
 
-        // Verify the results
+
     }
 
     @Test
-    void testSearch() {
-        // Setup
-        when(mockAdService.search(Zone.EarlyMorning, List.of(0), "auth")).thenReturn(List.of(new AdRepresentation()));
+    public void tesGetAdExternalServiceException() {
+        when(userService.verifyAuth("auth")).thenThrow(new ExternalServiceException("Problem on reaching user api."));
 
-        // Run the test
-        final Response result = adResourceUnderTest.search(Zone.EarlyMorning, "1001", "auth");
-
-        // Verify the results
+        String url = Fixture.API_ROOT + Fixture.ADS + "/1";
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .when().get(url)
+                .then().statusCode(424);
     }
 
     @Test
-    void testSearch_AdServiceReturnsNoItems() {
-        // Setup
-        when(mockAdService.search(Zone.EarlyMorning, List.of(0), "auth")).thenReturn(Collections.emptyList());
+    public void tesGetAdRadioException() {
+        when(userService.verifyAuth("auth")).thenThrow(new RadioException("auth error", 403));
 
-        // Run the test
-        final Response result = adResourceUnderTest.search(Zone.EarlyMorning, "1001", "auth");
-
-        // Verify the results
+        String url = Fixture.API_ROOT + Fixture.ADS + "/1";
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .when().get(url)
+                .then().statusCode(403);
     }
+
 
     @Test
     void testCreateAd() {
-        // Setup
-        final AdInputDTO adRepresentation = new AdInputDTO();
+//        AdInputDTO adInputDTO = new AdInputDTO();
+        AdRepresentation ad = new AdRepresentation();
+        ad.timezone = Zone.EarlyMorning;
+        ad.repPerZone = 1;
+        ad.duration = 3;
+        ad.startingDate = "01-01-2022";
+        ad.endingDate = "02-12-2022";
+        int numOfAds = adRepository.listAll().size();
 
-        // Configure AdService.create(...).
-        final Ad ad = new Ad(0, 0, LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 1), Zone.EarlyMorning);
-        when(mockAdService.create(any(AdRepresentation.class), eq("auth"))).thenReturn(ad);
+        String url = Fixture.API_ROOT + Fixture.ADS;
+        AdRepresentation adCreated = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .body(ad)
+                .when().post(url)
+                .then().statusCode(Response.Status.CREATED.getStatusCode())
+                .extract().as(AdRepresentation.class);
 
-        when(mockAdMapper.toRepresentation(any(Ad.class))).thenReturn(new AdRepresentation());
-
-        // Run the test
-        final Response result = adResourceUnderTest.createAd(adRepresentation, "auth");
-
-        // Verify the results
+        assertNotNull(adCreated);
+        assertTrue(ad.repPerZone.equals(adCreated.repPerZone));
+        assertTrue(ad.startingDate.equals(adCreated.startingDate));
+        assertTrue(ad.endingDate.equals(adCreated.endingDate));
+        assertEquals(numOfAds + 1, adRepository.listAll().size());
     }
 
     @Test
-    void testUpdateAd() {
-        // Setup
-        final AdInputDTO adRepresentation = new AdInputDTO();
+    public void testCreateBadRequest() {
 
-        // Run the test
-        final Response result = adResourceUnderTest.updateAd(0, adRepresentation, "auth");
+        // test radio exception
+        doThrow(new RadioException("Invalid ad input")).when(userService).verifyAuth(any(String.class));
+        AdInputDTO ad1 = new AdInputDTO();
+        String url = Fixture.API_ROOT + Fixture.ADS;
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .body(ad1)
+                .when().post(url)
+                .then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    }
 
-        // Verify the results
-        verify(mockAdService).update(eq(0), any(AdRepresentation.class), eq("auth"));
+        @Test
+        public void tesCreateRadioException() {
+        // test external service exception
+            String url = Fixture.API_ROOT + Fixture.ADS;
+        AdInputDTO ad2 = new AdInputDTO();
+        ad2.timezone = Zone.EarlyMorning;
+        ad2.repPerZone = 1;
+        ad2.duration = 3;
+        when(userService.verifyAuth("auth")).thenThrow(new ExternalServiceException("Problem on reaching user api."));
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .body(ad2)
+                .when().post(url)
+                .then().statusCode(424);
     }
 
     @Test
-    void testDeleteAd() {
-        // Setup
-        // Run the test
-        final Response result = adResourceUnderTest.deleteAd(0, "auth");
-
-        // Verify the results
-        verify(mockAdService).delete(0, "auth");
+    public void searchAdFilteredTest() {
+        String url = Fixture.API_ROOT + Fixture.ADS + "/";
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .queryParam("adsIds", "1001")
+                .when()
+                .get(url)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode()).extract();
     }
+
+    @Test
+    public void testSearchWithFiltersRadioException() {
+        when(userService.verifyAuth("auth")).thenThrow(new RadioException("auth error", 403));
+
+        String url = Fixture.API_ROOT + Fixture.ADS + "/";
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "auth")
+                .when().get(url)
+                .then().statusCode(403);
+    }
+
 }
+
+
