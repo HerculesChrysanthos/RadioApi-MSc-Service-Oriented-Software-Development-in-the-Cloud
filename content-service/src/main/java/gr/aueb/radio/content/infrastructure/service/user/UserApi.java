13c4616b54086fe222ac9c81e0966f85c46f4f4f
despknkd <des.kanakidou@gmail.com>
2024-03-11 19:49:11 +0200

    Content service - User api fault tolerance

--- content-service/src/main/java/gr/aueb/radio/content/infrastructure/service/user/UserApi.java
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/service/user/UserApi.java
@@ -3,8 +3,11 @@ package gr.aueb.radio.content.infrastructure.service.user;
 import gr.aueb.radio.content.infrastructure.service.user.representation.UserVerifiedRepresentation;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.time.temporal.ChronoUnit;
 
 @Path("/api/users")
 @ApplicationScoped
@@ -13,6 +16,9 @@ public interface UserApi {
 
     @GET
     @Path("/verify-auth")
+    @Timeout(500)
+    @Retry(maxRetries = 4, delay = 1000,
+            delayUnit = ChronoUnit.MILLIS)
     UserVerifiedRepresentation verifyAuth(
             @HeaderParam("Authorization") String basicAuthHeader
     );
