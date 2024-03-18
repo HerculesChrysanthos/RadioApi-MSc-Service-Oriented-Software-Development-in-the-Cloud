1e0b2457da3c43506df6e72614cebed26d015953
HerculesChrysanthos <hercsma@hotmail.com>
2024-03-18 19:18:52 +0200

    Add circuit breaker on broadcast when calling user api

--- broadcast-service/src/main/java/gr/aueb/radio/broadcast/infrastructure/service/user/UserServiceImpl.java
+++ broadcast-service/src/main/java/gr/aueb/radio/broadcast/infrastructure/service/user/UserServiceImpl.java
@@ -8,6 +8,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.WebApplicationException;
+import org.eclipse.microprofile.faulttolerance.exceptions.CircuitBreakerOpenException;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
@@ -33,6 +34,9 @@ public class UserServiceImpl implements UserService {
             throw new ExternalServiceException("Problem on reaching user api.");
         } catch (WebApplicationException webApplicationException){
             throw new RadioException("Auth error", webApplicationException.getResponse().getStatus());
+        } catch (CircuitBreakerOpenException circuitBreakerOpenException) {
+            throw new RadioException("User api keeps getting errors", 422);
+
         }
 
     }
