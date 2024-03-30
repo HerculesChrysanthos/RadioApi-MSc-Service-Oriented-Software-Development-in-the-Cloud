55a80c61c10fb8bb2d9472111aad689465f38f8d
HerculesChrysanthos <hercsma@hotmail.com>
2024-03-30 12:29:00 +0200

    Change default value about memory health check

--- user-service/src/main/java/gr/aueb/radio/user/infrastructure/rest/resource/health/OSHealthCheck.java
+++ user-service/src/main/java/gr/aueb/radio/user/infrastructure/rest/resource/health/OSHealthCheck.java
@@ -9,55 +9,11 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Liveness;
 
-//@Liveness
-//@ApplicationScoped
-//public class LivenessHealthCheck implements HealthCheck {
-//
-////    @Inject
-////    UriInfo uriInfo;
-//
-////    @Context
-////    UriInfo uriInfo;\
-//
-//    @Override
-//    public HealthCheckResponse call() {
-//        // Check if the application is live
-//        if(isApplicationLive()) {
-//
-//            return HealthCheckResponse.up("Application is live");
-//        }
-//        return HealthCheckResponse.up("Application is not live");
-//    }
-//
-//    public boolean isApplicationLive() {
-//       // String endpoint = uriInfo.getPath();// + "/api/check-auth" ;
-//
-//        String relativePath = uriInfo.getPath();
-//
-//        String e = "https:localhost:8080/api/check-auth";
-//
-////        try {
-////            URL url = new URL(e);
-////            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-////            connection.setRequestMethod("GET");
-////            int responseCode = connection.getResponseCode();
-////            return responseCode == 401;
-////        } catch (IOException er) {
-////            // Log or handle the exception if necessary
-////            return false;
-////        }
-//        return false;
-//    }
-//
-//}
-
-
-// TODO alternative : create user client and call it
 @Liveness
 @ApplicationScoped
 public class OSHealthCheck implements HealthCheck {
     @Inject
-    @ConfigProperty(name = "freememory.threshold", defaultValue = "10485760")
+    @ConfigProperty(name = "freememory.threshold", defaultValue = " 42949672964294967296") // 4gb
     private long threshold;
     @Override
     public HealthCheckResponse call() {
