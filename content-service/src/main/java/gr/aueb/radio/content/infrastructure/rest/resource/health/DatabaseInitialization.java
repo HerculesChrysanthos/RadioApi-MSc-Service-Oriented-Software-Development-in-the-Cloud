410ffa7b128f8c3993489f8519770bed352ab450
HerculesChrysanthos <hercsma@hotmail.com>
2024-03-30 12:51:14 +0200

    Add content service health checks

---
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/rest/resource/health/DatabaseInitialization.java
@@ -0,0 +1,28 @@
+package gr.aueb.radio.content.infrastructure.rest.resource.health;
+
+import gr.aueb.radio.content.infrastructure.persistence.SongRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Startup;
+
+@ApplicationScoped
+@Startup
+public class DatabaseInitialization implements HealthCheck {
+
+    @Inject
+    SongRepository songRepository;
+
+    @Override
+    public HealthCheckResponse call() {
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Startup check");
+
+        if(songRepository.I){
+            return responseBuilder.up().withData("status", "UP").build();
+        }
+
+        return responseBuilder.down().withData("status", "DOWN").build();
+    }
+}
---
+++ content-service/src/main/java/gr/aueb/radio/content/infrastructure/rest/resource/health/OSHealthCheck.java
@@ -0,0 +1,29 @@
+package gr.aueb.radio.content.infrastructure.rest.resource.health;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class OSHealthCheck implements HealthCheck {
+    @Inject
+    @ConfigProperty(name = "freememory.threshold", defaultValue = "10485760") // 10mb
+    private long threshold;
+    @Override
+    public HealthCheckResponse call() {
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("MemoryHealthCheck Liveness check");
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        if (freeMemory >= threshold) {
+            responseBuilder.up().withData("status", "UP");
+        } else {
+            responseBuilder.down().withData("error",
+                    "Not enough free memory! Available " + freeMemory + "Please restart application");
+        }
+        return responseBuilder.build();
+    }
+}
