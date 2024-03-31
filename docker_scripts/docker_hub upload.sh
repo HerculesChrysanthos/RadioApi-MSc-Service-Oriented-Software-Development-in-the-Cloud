f48b999d71fb4a9a649dc1a1c81c763ecd0ab7ff
HerculesChrysanthos <hercsma@hotmail.com>
2024-03-31 22:59:45 +0300

    Update configs

--- docker_scripts/docker_hub upload.sh
+++ docker_scripts/docker_hub upload.sh
@@ -5,3 +5,11 @@ cd content-service
 docker login
 docker tag hercules/content-service:1.0 herculeschrysanthos/content:1.0
 docker push herculeschrysanthos/content:1.0
+#
+#cd ..
+#cd user-service
+#
+#
+#docker login
+#docker tag hercules/user-service:1.0 herculeschrysanthos/user:1.0
+#docker push herculeschrysanthos/user:1.0
