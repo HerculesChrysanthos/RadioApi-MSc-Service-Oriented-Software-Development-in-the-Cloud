ccb853769673dec3f6fb7718548212951405ca33
HerculesChrysanthos <hercsma@hotmail.com>
2024-03-31 23:27:56 +0300

    Update configs

--- docker_scripts/docker_hub upload.sh
+++ docker_scripts/docker_hub upload.sh
@@ -1,10 +1,10 @@
-cd ..
-cd content-service
-
-
-docker login
-docker tag hercules/content-service:1.0 herculeschrysanthos/content:1.0
-docker push herculeschrysanthos/content:1.0
+#cd ..
+#cd content-service
+#
+#
+#docker login
+#docker tag hercules/content-service:1.0 herculeschrysanthos/content:1.0
+#docker push herculeschrysanthos/content:1.0
 #
 #cd ..
 #cd user-service
@@ -13,3 +13,11 @@ docker push herculeschrysanthos/content:1.0
 #docker login
 #docker tag hercules/user-service:1.0 herculeschrysanthos/user:1.0
 #docker push herculeschrysanthos/user:1.0
+
+cd ..
+cd broadcast-service
+
+
+docker login
+docker tag hercules/broadcast-service:1.0 herculeschrysanthos/broadcast:1.0
+docker push herculeschrysanthos/broadcast:1.0
