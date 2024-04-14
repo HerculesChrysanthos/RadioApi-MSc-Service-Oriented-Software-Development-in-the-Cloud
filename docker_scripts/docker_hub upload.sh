5d3e2f5f15046afaff4dea8f3072c7368bdafd9d
HerculesChrysanthos <hercsma@hotmail.com>
2024-04-14 12:58:30 +0300

    Add changes

--- docker_scripts/docker_hub upload.sh
+++ docker_scripts/docker_hub upload.sh
@@ -6,18 +6,18 @@
 #docker tag hercules/content-service:1.0 herculeschrysanthos/content:1.0
 #docker push herculeschrysanthos/content:1.0
 #
-#cd ..
-#cd user-service
-#
-#
-#docker login
-#docker tag hercules/user-service:1.0 herculeschrysanthos/user:1.0
-#docker push herculeschrysanthos/user:1.0
-
 cd ..
-cd broadcast-service
+cd user-service
 
 
 docker login
-docker tag hercules/broadcast-service:1.0 herculeschrysanthos/broadcast:1.0
-docker push herculeschrysanthos/broadcast:1.0
+docker tag hercules/user-service:1.0 herculeschrysanthos/user:1.0
+docker push herculeschrysanthos/user:1.0
+
+#cd ..
+#cd broadcast-service
+#
+#
+#docker login
+#docker tag hercules/broadcast-service:1.0 herculeschrysanthos/broadcast:1.0
+#docker push herculeschrysanthos/broadcast:1.0
